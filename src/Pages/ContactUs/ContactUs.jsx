09b5d2660b7280af24b6e87c8d4ef7eabacf8421
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-07-12 01:47:57 +0200

    Working on the Contact Us Component

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,4 +1,4 @@
-import image from "../../assets/contactus.png";
+import image from "/src/assets/hero3.jpg";
 
 const ContactUs = () => {
   return (
@@ -7,16 +7,18 @@ const ContactUs = () => {
         <h1 className="text-3xl font-bold text-center pb-3">
           Get In Touch With Us
         </h1>
-        <p className="text-lg text-center pb-10 md:px-[10rem] px-1">
+        <p className="text-lg text-center pb-14 md:px-[10rem] px-1">
           We welcome your inquiries and feedback. Please feel free to reach out
           to us with any questions, suggestions, or collaboration opportunities.
           Your input is invaluable as we strive to better serve you and our
           community.
         </p>
 
-        <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
-          <div className="w-full md:h-[27rem] h-[50vh] bg-red-400"></div>
-          <form action="" className="w-full shadow-lg">
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-6 shadow-2xl shadow-slate-300">
+          <div className="w-full md:h-[30rem] h-[40vh] bg-red-400 overflow-hidden rounded-md">
+            <img src={image} alt="" className="w-full h-full object-cover" />
+          </div>
+          <form action="" className="w-full md:px-4 px-1">
             <input
               type="text"
               placeholder="Enter your name"
@@ -38,7 +40,7 @@ const ContactUs = () => {
 
             <button
               type="submit"
-              className="py-2 px-6 bg-blue-300 text-xl text-white border-none rounded-xl hover:bg-opacity-70"
+              className="py-2 px-6 bg-blue-300 text-xl text-white border-none rounded-xl hover:bg-opacity-70 mb-5"
             >
               Submit
             </button>
