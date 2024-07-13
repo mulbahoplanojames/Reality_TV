a9978a795895b6212280dd579bc86e02771072c7
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-07-14 01:50:36 +0200

    Working on the Fruits Display Component

--- src/Pages/Nomination/Nomination.jsx
+++ src/Pages/Nomination/Nomination.jsx
@@ -6,15 +6,21 @@ const Nomination = () => {
   const { darkMode } = useContext(AppContext);
   return (
     <>
-      <div className="pt-32 px-3 bg-black">
+      <div className="pt-32 px-3">
         <h1 className="text-center text-3xl uppercase text-white">
           Please fill in this form to nominate a person
         </h1>
 
-        <FormfacadeEmbed
-          formFacadeURL="https://formfacade.com/include/108351829281762739211/form/1FAIpQLSe6iWzDKwaVqepE1vVclDnbq3yeUuKif1ApssksouRZ586X0Q/classic.js/?div=ff-compose"
-          onSubmitForm={() => console.log("Form submitted")}
-        />
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSe6iWzDKwaVqepE1vVclDnbq3yeUuKif1ApssksouRZ586X0Q/viewform?embedded=true"
+          width="100%"
+          height="569"
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
+        >
+          Loading…
+        </iframe>
 
         {/* <h1 className="text-center text-3xl py-5 md:pb-[9rem]">
           Nomination Portal will be Out Soon
