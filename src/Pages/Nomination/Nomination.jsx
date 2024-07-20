6b755e771092f6ec4d2c1d06c9ffd5b0ac361fdc
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-07-20 08:30:07 +0200

    Made some changes in the Sign Up, Forget Password, Reset Password and AuthContext components

--- src/Pages/Nomination/Nomination.jsx
+++ src/Pages/Nomination/Nomination.jsx
@@ -7,18 +7,18 @@ const Nomination = () => {
   return (
     <>
       <div className="pt-32 px-3 bg-[#162017]">
-        <h1 className="text-center text-3xl uppercase text-white">
+        {/* <h1 className="text-center text-3xl uppercase text-white">
           Please fill in this form to nominate a person
-        </h1>
+        </h1> */}
 
-        <FormfacadeEmbed
+        {/* <FormfacadeEmbed
           formFacadeURL="https://formfacade.com/include/108351829281762739211/form/1FAIpQLSe6iWzDKwaVqepE1vVclDnbq3yeUuKif1ApssksouRZ586X0Q/classic.js/?div=ff-compose"
           onSubmitForm={() => console.log("Form submitted")}
-        />
+        /> */}
 
-        {/* <h1 className="text-center text-3xl py-5 md:pb-[9rem]">
+        <h1 className="text-center text-3xl py-5 md:pb-[9rem]">
           Nomination Portal will be Out Soon
-        </h1> */}
+        </h1>
       </div>
     </>
   );
